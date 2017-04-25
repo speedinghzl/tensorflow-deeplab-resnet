bc87c676ef670313d5ee93b2ca1a8a6c13e8d063
speedinghzl <speedinghzl@gmail.com>
2017-04-25 10:52:06 +0800

    add refinenet model

--- deeplab_resnet/__init__.py
+++ deeplab_resnet/__init__.py
@@ -1,3 +1,4 @@
 from .model import DeepLabResNetModel
+from .model_unet import RefineResNetModel
 from .image_reader import ImageReader
 from .utils import decode_labels, inv_preprocess, prepare_label
--- kaffe/tensorflow/network.py
+++ kaffe/tensorflow/network.py
@@ -32,7 +32,7 @@ def layer(op):
 
 class Network(object):
 
-    def __init__(self, inputs, trainable=True, is_training=False):
+    def __init__(self, inputs, trainable=True, is_training=False, num_classes=21):
         # The input nodes for this network
         self.inputs = inputs
         # The current list of terminal nodes
@@ -45,7 +45,7 @@ class Network(object):
         self.use_dropout = tf.placeholder_with_default(tf.constant(1.0),
                                                        shape=[],
                                                        name='use_dropout')
-        self.setup(is_training)
+        self.setup(is_training, num_classes)
 
     def setup(self, is_training):
         '''Construct the network. '''
@@ -96,7 +96,7 @@ class Network(object):
 
     def make_var(self, name, shape):
         '''Creates a new TensorFlow variable.'''
-        return tf.get_variable(name, shape, trainable=self.trainable)
+        return tf.get_variable(name, shape, trainable=self.trainable, initializer=tf.contrib.layers.xavier_initializer())
 
     def validate_padding(self, padding):
         '''Verifies that the padding is one of the supported ones.'''
@@ -104,6 +104,48 @@ class Network(object):
 
     @layer
     def conv(self,
+             input,
+             k_h,
+             k_w,
+             c_o,
+             s_h,
+             s_w,
+             name,
+             relu=True,
+             padding=DEFAULT_PADDING,
+             group=1,
+             biased=True):
+        # Verify that the padding is acceptable
+        self.validate_padding(padding)
+        # Get the number of channels in the input
+        c_i = input.get_shape()[-1]
+        # Verify that the grouping parameter is valid
+        assert c_i % group == 0
+        assert c_o % group == 0
+        # Convolution for a given input and kernel
+        convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
+        with tf.variable_scope(name) as scope:
+            kernel = self.make_var('weights', shape=[k_h, k_w, c_i / group, c_o])
+            if group == 1:
+                # This is the common-case. Convolve the input without any further complications.
+                output = convolve(input, kernel)
+            else:
+                # Split the input into groups and then convolve each of them independently
+                input_groups = tf.split(3, group, input)
+                kernel_groups = tf.split(3, group, kernel)
+                output_groups = [convolve(i, k) for i, k in zip(input_groups, kernel_groups)]
+                # Concatenate the groups
+                output = tf.concat(3, output_groups)
+            # Add the biases
+            if biased:
+                biases = self.make_var('biases', [c_o])
+                output = tf.nn.bias_add(output, biases)
+            if relu:
+                # ReLU non-linearity
+                output = tf.nn.relu(output, name=scope.name)
+            return output
+
+    def _conv(self,
              input,
              k_h,
              k_w,
@@ -273,3 +315,44 @@ class Network(object):
     def dropout(self, input, keep_prob, name):
         keep = 1 - self.use_dropout + (self.use_dropout * keep_prob)
         return tf.nn.dropout(input, keep, name=name)
+
+    @layer
+    def residual_conv_unit(self, input, name):
+        c_o = input.get_shape()[-1]
+        with tf.variable_scope(name) as scope:
+            output = tf.nn.relu(input, name='relu_0')
+            output = self._conv(output, 3, 3, c_o, 1, 1, name='conv_0', relu=False)
+            output = tf.nn.relu(output, name='relu_1')
+            output = self._conv(output, 3, 3, c_o, 1, 1, name='conv_1', relu=False, biased=False)
+            output = tf.add_n([input, output], name='add')
+
+        return output
+
+    @layer
+    def multi_scale_fusion(self, inputs, name):
+        c_i = inputs[0].get_shape()[-1]
+        c_o = inputs[1].get_shape()[-1]
+        output_shape = tf.shape(inputs[1])
+        with tf.variable_scope(name) as scope:
+            kernel = self.make_var('weights', shape=[3, 3, c_o, c_i])
+            stream0 = tf.nn.conv2d_transpose(inputs[0], kernel, output_shape,
+                                            strides=[1, 2, 2, 1], padding='SAME', name='stream0')
+
+            stream1 = self._conv(inputs[1], 3, 3, c_o, 1, 1, name='stream1', relu=False, biased=False)
+            output = tf.add_n([stream0, stream1], name='add')
+
+        return output
+
+    @layer
+    def chained_residual_pooling(self, input, name, num):
+        c_o = input.get_shape()[-1]
+        output_list = [input]
+        with tf.variable_scope(name) as scope:
+            for i in xrange(num):
+                output = tf.layers.max_pooling2d(input, (5, 5), (1, 1), name='chained_pool'+str(i), padding='same')
+                output = self._conv(output, 3, 3, c_o, 1, 1, relu=False, name='chained_conv'+str(i), biased=False)
+                output_list.append(output)
+
+            output = tf.add_n(output_list, name='add')
+
+        return output
